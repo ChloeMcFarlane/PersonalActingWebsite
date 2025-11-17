dac93e0f359293b2fd25d526a6f394f00364c3ba
ChloeMcFarlane <chloem627@gmail.com>
2025-11-17 22:53:33 +0000

    Creation and partial completion of the photos page - Nov 17. 2025

--- js/photos.js
+++ js/photos.js
@@ -1,10 +1,10 @@
+
 $(document).ready(function() {
 
   $("[unique-script-id='w-w-dm-id'] .btn-box").click(function() {
     $(this).parent().children(".overlay").show();
 
   });
-
 
   $("[unique-script-id='w-w-dm-id'] .close").click(function() {
     $(".overlay").hide();
@@ -13,8 +13,8 @@ $(document).ready(function() {
   $("[unique-script-id='w-w-dm-id'] .list").click(function() {
     const value = $(this).attr('data-filter');
     if (value == 'all') {
-      $("[unique-script-id='w-w-dm-id'] .squareImg").show('1000');
-    } else {
+      $("[unique-script-id='w-w-dm-id'] .squareImg ").show('1000');
+    } else{
       $("[unique-script-id='w-w-dm-id'] .squareImg").not('.' + value).hide('1000');
       $("[unique-script-id='w-w-dm-id'] .squareImg").filter('.' + value).show('1000');
     }
